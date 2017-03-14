74cbbef53b12c40d9a3c3e5ad741f2cafd94af84
agadar <martin.donkersloot@outlook.com>
2017-03-14 22:35:44 +0100

    Updated .gitignore, created HistoryManager class.
    TODO: Change code to use the HistoryManager.

--- NationStates-Telegrammer-CLI/src/main/java/com/github/agadar/telegrammer/cli/Main.java
+++ NationStates-Telegrammer-CLI/src/main/java/com/github/agadar/telegrammer/cli/Main.java
@@ -1,5 +1,9 @@
 package com.github.agadar.telegrammer.cli;
 
+import com.github.agadar.telegrammer.core.enums.SkippedRecipientReason;
+import com.github.agadar.telegrammer.core.manager.HistoryManager;
+import com.github.agadar.telegrammer.core.util.Tuple;
+
 /**
  *
  * @author Agadar (https://github.com/Agadar/)
@@ -10,7 +14,19 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        HistoryManager hm = HistoryManager.get();
+        
+        hm.loadHistory();
+        
+        
+        
+        hm.history.entrySet().forEach(entry -> {
+           System.out.println(entry.getKey().X + " " + entry.getKey().Y + " " + entry.getValue().name()); 
+        });
+        
+        hm.history.put(new Tuple("72383", "aggy1"), SkippedRecipientReason.NOT_FOUND);
+        
+        hm.saveHistory();
     }
     
 }
---
+++ NationStates-Telegrammer-Core/src/main/java/com/github/agadar/telegrammer/core/manager/HistoryManager.java
@@ -0,0 +1,106 @@
+package com.github.agadar.telegrammer.core.manager;
+
+import com.github.agadar.telegrammer.core.enums.SkippedRecipientReason;
+import com.github.agadar.telegrammer.core.util.Tuple;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Assists in saving and loading the history file for this application.
+ *
+ * @author Agadar (https://github.com/Agadar/)
+ */
+public class HistoryManager {
+
+    /**
+     * The singleton.
+     */
+    private static HistoryManager INSTANCE;
+
+    /**
+     * Default file name.
+     */
+    private static final String FILENAME = "nationstates-telegrammer.history";
+
+    /**
+     * Default split string.
+     */
+    private static final String SPLITSTRING = ",";
+
+    /**
+     * The history data retrieved from and saved to the file.
+     */
+    public Map<Tuple<String, String>, SkippedRecipientReason> history;
+
+    /**
+     * Gets this class's singleton.
+     *
+     * @return This class's singleton.
+     */
+    public static HistoryManager get() {
+        if (INSTANCE == null) {
+            INSTANCE = new HistoryManager();
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * Private constructor, because singleton.
+     */
+    private HistoryManager() {
+    }
+
+    /**
+     * Saves the application's history data to the file.
+     */
+    public void saveHistory() {
+        // If there is no history, don't even bother.
+        if (history == null || history.isEmpty()) {
+            return;
+        }
+
+        try {
+            // Parse all entries to a single string.
+            final String contents = history.entrySet().stream()
+                    .map(historyEntry -> historyEntry.getKey().X
+                            + SPLITSTRING + historyEntry.getKey().Y
+                            + SPLITSTRING + historyEntry.getValue().name())
+                    .collect(Collectors.joining(System.lineSeparator()));
+            Files.write(Paths.get(FILENAME), contents.getBytes()); // Write the resulting string to the file.
+        } catch (IOException ex) {
+            // Silently ignore, because this is optional anyway.
+        }
+    }
+
+    /**
+     * Loads the application's history data from the file.
+     */
+    public void loadHistory() {
+        history = new HashMap<>(); // Ensure history is new and empty.
+
+        // Break the history file contents into lines and iterate over them.
+        try (final Stream<String> lines = Files.lines(Paths.get(FILENAME), Charset.defaultCharset())) {
+            lines.map(line -> line.split(SPLITSTRING)).filter(splitLine -> splitLine.length >= 3).forEach(splitLine -> {
+                try {
+                    // Try parse the reason string to the correct type. If this succeeds, the line was succesfully parsed,
+                    // so we add it to the history.
+                    final SkippedRecipientReason reason = SkippedRecipientReason.valueOf(splitLine[2]);
+                    final Tuple<String, String> telegramIdAndRecipient = new Tuple<>(splitLine[0], splitLine[1]);
+                    history.put(telegramIdAndRecipient, reason);
+                } catch (IllegalArgumentException | NullPointerException ex) {
+                    // Failed to parse the reason. We simply skip this line.
+                }
+            });
+        } catch (IOException ex) {
+            // History file probably doesn't exist yet, but that's okay.
+        }
+    }
+}
